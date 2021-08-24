5ebfbc3c40e0b8d502f4dad428b4f10b0def5c4d
htruong19 <69446345+htruong19@users.noreply.github.com>
2021-08-24 02:40:10 -0400

    Adding Workshop 1

---
+++ Workshop 1 - Introduction/w1_home.c
@@ -0,0 +1,13 @@
+/*Workshop 1: Introduction - Home Portion
+Name: Hung Truong  || Student Number: 147779193 || Email: htruong19@myseneca.ca*/
+
+#include<stdio.h> 
+
+int main(void)
+{
+	printf("********************************\n"); //\n seperates the next printed statement onto a new line
+	printf("*** Welcome to C Programming ***\n"); //To print content within "" to screen
+	printf("********************************");
+
+	return 0;
+}
